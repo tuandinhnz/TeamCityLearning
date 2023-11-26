8b2a606cbfbc151055bc86de489a41ed398228e6
tuandinhnz <letuandinh@gmail.com>
2023-11-26 19:16:49 +1300

    Added unit tests

--- .idea/.idea.TeamCityLearning/.idea/sonarlint/issuestore/index.pb
+++ .idea/.idea.TeamCityLearning/.idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,5 @@
 
 K
 TeamCityLearning/Program.cs,e/f/efa513e384f784784da14d1e50da676e710bd3eb
+W
+'TeamCityLearning.UnitTests/UnitTest1.cs,b/f/bfae7d7d95c1dd74a8d6c3b02dcf17fd7d4fc3b6
---
+++ TeamCityLearning.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TeamCityLearning.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        const int number = 1;
+        Assert.Equal(0, number);
+    }
+}
